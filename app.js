aea8b86e3652f4e76bfef6a9fd38d718f7008730
Fahad Hussain <k163936@nu.edu.pk>
2019-04-26 15:17:16 +0500

    Update app.js
    With Upload picture function and post donation

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
+"use strict";
+
 const express = require('express');
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
 const app = express();
+const multer = require('multer');
 const port = process.env.PORT || 3000;
+const mysql = require('mysql')
 
 const passport = require('passport');
 const flash = require('connect-flash');
 
+// connect to database
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ZINDAGItrust",
+});
+
+connection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
+});
+
+global.connection = connection;
+
+//----------------------------------//
+
+// Setting Environments //
+
 require('./config/passport')(passport);
 
-// app.use(morgan('dev'));
+app.use(session({
+  secret: 'justasecret',
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -20,13 +50,6 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-
-app.use(session({
-  secret: 'justasecret',
-  resave: true,
-  saveUninitialized: true
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
